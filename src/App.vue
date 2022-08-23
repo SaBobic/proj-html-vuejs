f225dd28501d80648e90f05b131e69c9881868c9
SaBobic <fabrizioettori@gmail.com>
2022-08-23 15:11:16 +0200

    added header menu as component

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,73 @@
           <img src="./assets/img/theme_eduprime_logo.png" alt="EduPrime Logo">
         </div>
         <div class="d-flex align-items-center">
-          <ul class="main-menu d-flex">
-            <li>
-              <a href="#" class="active">Text</a>
-            </li>
-            <li><a href="#">Text</a></li>
-            <li><a href="#">Text</a></li>
-            <li><a href="#">Text</a></li>
-            <li><a href="#">Text</a></li>
-            <li><a href="#">Text</a></li>
-            <li><a href="#">Text</a></li>
-          </ul>
+          <RowMenu :items="mainMenuItems" />
           <a id="header-button" href="#">VIEW COURSES</a>
         </div>
       </nav>
     </header>
     <main class="bg-danger">
-
     </main>
   </div>
 </template>
 
 <script>
+import RowMenu from './components/RowMenu.vue';
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      mainMenuItems: [
+        {
+          id: 1,
+          anchor: 'Home',
+          link: '#',
+          sub: true,
+        },
+        {
+          id: 2,
+          anchor: 'Courses',
+          link: '#',
+          sub: true,
+        },
+        {
+          id: 3,
+          anchor: 'About Us',
+          link: '#',
+          sub: false,
+        },
+        {
+          id: 4,
+          anchor: 'News',
+          link: '#',
+          sub: true,
+        },
+        {
+          id: 5,
+          anchor: 'Pages',
+          link: '#',
+          sub: true,
+        },
+        {
+          id: 6,
+          anchor: 'Contact',
+          link: '#',
+          sub: false,
+        },
+        {
+          id: 7,
+          anchor: 'Purchase',
+          link: '#',
+          sub: false,
+        },
+      ]
+    }
+  },
+  components: {
+    RowMenu,
+  }
 }
 </script>
 
