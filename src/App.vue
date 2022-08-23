a8fb9f12fa8b815141af3d0060db8197bbf5dee8
SaBobic <fabrizioettori@gmail.com>
2022-08-23 13:36:04 +0200

    added raw header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
   <div id="app">
+    <header>
+      <nav class="d-flex justify-content-between align-items-center">
+        <div>
+          <img src="./assets/img/theme_eduprime_logo.png" alt="EduPrime Logo">
+        </div>
+        <div class="d-flex align-items-center">
+          <ul class="main-menu d-flex">
+            <li>
+              <a href="#" class="active">Text</a>
+            </li>
+            <li><a href="#">Text</a></li>
+            <li><a href="#">Text</a></li>
+            <li><a href="#">Text</a></li>
+            <li><a href="#">Text</a></li>
+            <li><a href="#">Text</a></li>
+            <li><a href="#">Text</a></li>
+          </ul>
+          <a id="header-button" href="#">VIEW COURSES</a>
+        </div>
+      </nav>
+    </header>
+    <main class="bg-danger">
+
+    </main>
   </div>
 </template>
 
