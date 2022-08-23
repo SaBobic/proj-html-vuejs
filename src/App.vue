c3b081e57ffb461a494fb217c2023b263fa5b901
SaBobic <fabrizioettori@gmail.com>
2022-08-23 17:02:05 +0200

    added jumbotron section

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- Header -->
     <header>
       <nav class="d-flex justify-content-between align-items-center">
         <div>
@@ -7,11 +8,29 @@
         </div>
         <div class="d-flex align-items-center">
           <RowMenu :items="mainMenuItems" />
-          <a id="header-button" href="#">VIEW COURSES</a>
+          <a class="btn-primary" id="header-button" href="#">VIEW COURSES</a>
         </div>
       </nav>
     </header>
-    <main class="bg-danger">
+    <!-- Main -->
+    <main>
+      <section class="jumbotron d-flex justify-content-center align-items-center">
+        <div class="jumbo-content">
+          <h2>Key to your success</h2>
+          <p>EduPrime is the most versatile Wordpress theme for educational purposes, showcasing universities,
+            courses,
+            secondary schools etc.</p>
+          <a class="btn-primary" href="#">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <span>Search courses</span>
+          </a>
+          <a class="btn-white" href="#">
+            <i class="fa-solid fa-user-plus"></i>
+            <span>Apply for university</span>
+          </a>
+        </div>
+        <img src="./assets/img/Wave-1.png" alt="">
+      </section>
     </main>
   </div>
 </template>
